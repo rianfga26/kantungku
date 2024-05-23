d4dc2f3b3f1c7f6147b214da2da490bf496f6854
nfl001 <naufaladisatrio001@gmail.com>
2024-05-23 12:59:49 +0700

    change folder name

--- web/landing/src/components/home/Home.tsx
+++ web/landing/src/components/home/Home.tsx
@@ -1,14 +1,14 @@
-
 import Banner from "../../assets/home/demoapk.png";
 import Track from "../../assets/fitur/track.png";
 import Budget from "../../assets/fitur/budget.png";
-import AiSist from "../../assets/fitur/ai.png";
+import AiSist from "../../assets/fitur/ai.svg";
 import Card from "../card";
 import AppLayout from "../template/AppLayout";
 import CardFounder from "../cardFounder";
 import Galeh from "../../assets/founder/galeh.jpg";
 import Rio from "../../assets/founder/rio.jpg";
 import Rian from "../../assets/founder/rian.jpg";
+import { Icon } from "@iconify/react";
 
 export default function Home() {
   return (
@@ -61,7 +61,7 @@ export default function Home() {
             <Card title="budgeting" image={Budget}>
               <p>Group your budget easily using Kantungku</p>
             </Card>
-            <Card title="AI assistant" image={AiSist}>
+            <Card title="AI assistant" image={AiSist} >
               <p>
                 Kantungku AI Assistant makes personal finance fun and easy to
                 understand.
@@ -86,7 +86,9 @@ export default function Home() {
               title="Founder"
               image={Galeh}
             >
-              <p>With passion of innovation and drive to make different, he lead the team to achieve the vision of kantungku. 
+              <p>
+                With passion of innovation and drive to make different, he lead
+                the team to achieve the vision of kantungku.
               </p>
             </CardFounder>
             <CardFounder
@@ -94,36 +96,74 @@ export default function Home() {
               title="Bussiness Director"
               image={Rio}
             >
-              <p>Guiding kantungku through dynamic market and strategic planning to make sure our operation and our goals are met.</p>
+              <p>
+                Guiding kantungku through dynamic market and strategic planning
+                to make sure our operation and our goals are met.
+              </p>
             </CardFounder>
             <CardFounder
               name="Rian Firgiawan Gusti Ananda"
               title="Technology Director"
               image={Rian}
             >
-              <p>Kantungku tech wizard with creative problem solving and experience with cutting-edge technology push our product for the better.</p>
+              <p>
+                Kantungku tech wizard with creative problem solving and
+                experience with cutting-edge technology push our product for the
+                better.
+              </p>
             </CardFounder>
           </div>
-        </div>  
+        </div>
       </div>
       {/* Contact Us */}
-      <div className="hero min-h-screen bg-base p-16">
+      <div className="hero   bg-base p-16">
         <div className="hero-content flex-col w-full">
-            <p className="text-3xl font-semibold"><span className="text-primary font-bold">Chat</span> to our friendly team</p>
-            <div className="flex justify-around space-x-24 mt-12">
-                <div className="text-center">
-                   <span className="w-36 h-36 bg-primary rounded-full p-2">📧</span>
-                   <p>Our team is ready to help you</p>  
-                   <p>halo@kantungku.id</p>
-                </div>
-                <div className="text-center">
-                <span className="w-36 h-36 bg-primary rounded-full p-2">📞</span>
-                   <p>Our team is ready to help you</p>  
-                   <p>+62 8580-6926-548</p>
-                </div>
+          <p className="text-3xl font-semibold">
+            <span className="text-primary font-bold">Chat</span> to our friendly
+            team
+          </p>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+            <div className="flex gap-5">
+              <div className="bg-primary w-fit h-fit rounded-full p-2">
+                <Icon icon="lucide:mail" color="white" width={24} />
+              </div>
+              <div>
+                <p>Our team is ready to help you</p>
+                <a
+                  href="mailto:halo@kantungku.id"
+                  className="hover:text-primary"
+                >
+                  halo@kantungku.id
+                </a>
+              </div>
             </div>
-            <span className="bg-primary rounded-full p-2 mt-12">🪧</span>
-            <p className="w-80 text-center">Address: Penambangan Plumpang Balongbendo Sidoarjo Jawa timur 61623 Indonesia</p>
+            <div className="flex gap-5">
+              <div className="bg-primary w-fit h-fit rounded-full p-2">
+                <Icon icon="lucide:phone" color="white" width={24} />
+              </div>
+              <div>
+                <p>Our team is ready to help you</p>
+                <a
+                  href="https://wa.me/6285806926548"
+                  className="hover:text-primary"
+                >
+                  +62 8580-6926-548
+                </a>
+              </div>
+            </div>
+            <div className="flex gap-5">
+              <div className="bg-primary w-fit h-fit rounded-full p-2">
+                <Icon icon="lucide:signpost-big" color="white" width={24} />
+              </div>
+              <div>
+                <p>Kantungku Headquarter</p>
+                <p>
+                  Penambangan Plumpang Balongbendo Sidoarjo Jawa timur 61623
+                  Indonesia
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </AppLayout>
